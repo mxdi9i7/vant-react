bfcbb509f517fb7fae572be78cd14d6a0a74b302
mxdi9i7 <peter.zheng88228@gmail.com>
2020-05-27 09:32:12 -0400

    added export

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,20 @@ import Button from './components/Button';
 import Icon from './components/Icons';
 import Tag from './components/Tag';
 import Navbar from './components/Navbar';
+import Field from './components/Field';
 
 export { default as Button } from './components/Button';
 export { default as Icon } from './components/Icons';
 export { default as Tag } from './components/Tag';
 export { default as Navbar } from './components/Navbar';
+export { default as Field } from './components/Field';
 
 const Vant = {
   Button,
   Icon,
   Tag,
-  Navbar
+  Navbar,
+  Field
 };
 
 export default Vant;
