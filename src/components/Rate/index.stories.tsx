aa2319c44f62b12d59cb6a7b0ecc406b3518392e
mxdi9i7 <peter.zheng88228@gmail.com>
2020-05-30 12:42:16 -0400

    wip

--- src/components/Rate/index.stories.tsx
+++ src/components/Rate/index.stories.tsx
@@ -9,6 +9,18 @@ export default {
 
 export const BasicUsage = () => (
   <div className='container column grey'>
-    <Rate currentRate={4} disabled />
+    <Rate currentRate={4} />
+  </div>
+);
+
+export const CustomIcon = () => (
+  <div className='container column grey'>
+    <Rate currentRate={4} icon='like' voidIcon='like-o' />
+  </div>
+);
+
+export const CustomColor = () => (
+  <div className='container column grey'>
+    <Rate currentRate={4} icon='like' voidIcon='like-o' color='#1989fa' />
   </div>
 );
