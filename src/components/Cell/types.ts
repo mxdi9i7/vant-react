dddea7b32ca996e3ff60e64a0736deb6958bf9e4
wsh941102 <willsihangw@gmail.com>
2020-07-02 16:36:21 -0400

    fixed cell bugs

--- src/components/Cell/types.ts
+++ src/components/Cell/types.ts
@@ -3,9 +3,9 @@ import { ReactElement } from 'react';
 export interface IProps {
   title?: { text: string; fontSize: string };
   titleIcon?: { name: string; size: string };
-	content?: { text: string, fontSize: string; };
+  content?: { text: string; fontSize: string };
   contentIcon?: { name: string; size: string };
-  description?: string;
+  description?: { text: string; fontSize: string };
   Tag?: ReactElement;
   url?: string;
   replace?: boolean;
