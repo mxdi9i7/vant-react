8aef70d6b34da87a440e8f8e75469d014ad8237e
guojiankun <2291092612@qq.com>
2021-05-16 16:20:08 +0800

    docs: add different Loading storybook examples

--- src/components/Loading/index.stories.tsx
+++ src/components/Loading/index.stories.tsx
@@ -13,16 +13,50 @@ export const BasicUsage = () => {
     <div>
       <Loading />
       <Loading type='spinner' />
-      <Loading type='spinner' color='#1989fa' size='40px' />
+    </div>
+  );
+};
+
+export const LoadingText = () => {
+  return (
+    <div>
       <Loading text='加载中...' />
       <Loading type='spinner' text='加载中...' />
-      <Loading type='spinner' text='加载中...' vertical />
+    </div>
+  );
+};
+
+export const LoadingColor = () => {
+  return (
+    <div>
+      <Loading color='#1989fa' />
+      <Loading type='spinner' color='#1989fa' />
       <Loading
         type='spinner'
         text='加载中...'
         color='#1989fa'
-        textColor='#1989fa'
+        textColor='#DC143C'
       />
+    </div>
+  );
+};
+
+export const LoadingSize = () => {
+  return (
+    <div>
+      <Loading size='40px' />
+      <Loading type='spinner' size='50px' />
+      <Loading type='spinner' text='加载中...' size='60px' />
+    </div>
+  );
+};
+
+export const LoadingVertical = () => {
+  return (
+    <div>
+      <Loading vertical />
+      <Loading type='spinner' vertical />
+      <Loading type='spinner' text='加载中...' vertical />
     </div>
   );
 };
